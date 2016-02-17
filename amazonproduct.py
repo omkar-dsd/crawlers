16d036d7adc9a6a303c483d6ff8cccd0e76ce0ba
omkar-dsd <omkar.dsd@gmail.com>
2016-02-17 21:46:58 +0530

    Updating amazonproduct.py file

--- amazonproduct.py
+++ amazonproduct.py
@@ -1,40 +1,39 @@
+from bs4 import BeautifulSoup as bs
 from pymongo import MongoClient
-from bs4 import BeautifulSoup
 import urllib2
 
-#----- PROXY HANDLER -------
+# PROXY HANDLER----
+
 proxy=urllib2.ProxyHandler({})
 opener=urllib2.build_opener(proxy)
 opener.addheaders=[('User-agent','Mozilla/5.0')]
 urllib2.install_opener(opener)
-#----- END PROXY HANDLER ------
+
+# PROXY HANDLER END -----------
 
 
-#----- Function to extract links from page ----
+# LINK EXTRACTOR FUNCTION-----
 
-def linkExtractor(urltoopen, tag1, attrb1, attrib1value, tag2, attrb2, attrib2value, finalattrib):
+def linkExtractor(urltoopen, tag1, attrib1, attrib1value, tag2 ,attrib2, attrib2value, finalAttrib):
+	url = urllib2.urlopen(urltoopen).read()
+	soup = bs(url)
 
-	urlhtmlreader = urllib2.urlopen(urltoopen).read()
-	soup = BeautifulSoup(urlhtmlreader)
-
-
-	result = soup.findAll(tag1,{attrb1:attrib1value})
+	result = soup.findAll(tag1,{attrib1:attrib1value})
 	hreflist = []
 
 	for i in range(0,len(result)):
+		resultDetails = result[i].find(tag2,{attrib2:attrib2value})
+		link = resultDetails[finalAttrib]
+		hreflist.append(link)      # appends links into array
+		
+	return hreflist     # returns list of fetched links
 
-		resultDetails = result[i].find(tag2,{attrb2:attrib2value})
-		link = resultDetails(finalattrib)
-		hreflist.append(link)              # adding links to arrays
+#FUNCTION END----------
 
-	return hreflist   # returning the arraylist containing links
-
-# ----- Function END
-
+def forPrinter(x):
+	for i in range(1,len(x)):
+		print x[i]
 
 url = "http://www.amazon.com/s/ref=nb_sb_noss_2/185-5945162-0333204?url=search-alias%3Daps&field-keywords=laptop"
 links = linkExtractor(url,"li","class","s-result-item","a","class","a-link-normal","href")
-for i in range(0,len(links)):
-
-	print i
-	print links[i]
+forPrinter(links)
